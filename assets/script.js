edba9fd3e392d295b590294c8b567e4469afbf67
prosg <learner.in.dev@gmail.com>
2024-06-03 15:24:37 +0200

    ajout des fonctions et des event

--- assets/script.js
+++ assets/script.js
@@ -21,15 +21,44 @@ const slides = [
 let currentSlide = 0;
 
 const banner = document.getElementById("banner");
-const arrowPrevious = document.getElementById("arrowPrevious");
-const arrowNext = document.getElementById("arrowNext");
 const dotsContainer = document.querySelector(".dots");
+const arrowPrevious = document.querySelector(".arrow_left");
+const arrowNext = document.querySelector(".arrow_right");
+const image = document.querySelector(".banner-img");
+const tagLine = document.querySelector(".tagline");
 
-slides.forEach((slide, index) => {
-  const dotElement = document.createElement("div");
-  dotElement.classList.add("dot");
-  if (index == currentSlide) {
-    dotElement.classList.add("dot_selected");
-  }
-  dotsContainer.appendChild(dotElement);
+function createDots() {
+  slides.forEach((slide, index) => {
+    const dotElement = document.createElement("div");
+    dotElement.classList.add("dot");
+    if (index == currentSlide) {
+      dotElement.classList.add("dot_selected");
+    }
+    dotsContainer.appendChild(dotElement);
+  });
+}
+createDots();
+
+function updateDots() {
+  const dots = dotsContainer.querySelectorAll(".dot");
+  dots.forEach((dot, index) => {
+    dot.classList.toggle("dot_selected", index === currentSlide);
+  });
+}
+
+function updateSlide() {
+  image.src = "./assets/images/slideshow/" + slides[currentSlide].image;
+  tagLine.innerHTML = slides[currentSlide].tagLine;
+}
+
+arrowNext.addEventListener("click", () => {
+  currentSlide = (currentSlide + 1) % slides.length;
+  updateSlide();
+  updateDots();
+});
+
+arrowPrevious.addEventListener("click", () => {
+  currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+  updateSlide();
+  updateDots();
 });
